54ce0f702332d273266eabe78fcaba3c44f0aa59
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-10-13 12:05:29 -0400

    Simplified isLeapYear a little

--- lesson_4/summary/main.go
+++ lesson_4/summary/main.go
@@ -2,10 +2,7 @@ package main
 
 func isLeapYear(year int) bool {
 	if year%4 == 0 {
-		if year%100 == 0 {
-			if year%400 == 0 {
-				return true
-			}
+		if year%100 == 0 && year%400 != 0 {
 			return false
 		}
 		return true
