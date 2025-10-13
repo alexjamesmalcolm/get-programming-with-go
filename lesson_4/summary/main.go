e0875fe9d51a2698ee2f95ea966f17318c220fe8
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-10-13 12:04:15 -0400

    Started code for isLeapYear

---
+++ lesson_4/summary/main.go
@@ -0,0 +1,16 @@
+package main
+
+func isLeapYear(year int) bool {
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				return true
+			}
+			return false
+		}
+		return true
+	}
+	return false
+}
+
+func main() {}
