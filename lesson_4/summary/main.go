0e4398cade04d4abcbca977526af7ca186d3dedc
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-10-13 12:11:53 -0400

    Created getDaysInMonth which makes use of isLeapYear Also added comments to both functions

--- lesson_4/summary/main.go
+++ lesson_4/summary/main.go
@@ -1,5 +1,7 @@
 package main
 
+// Determines if a year is a leap year. If a year is divisible by 4 then it must also either not be
+// divisible by 100 or if it is divisible by 100 it has to also be divisible by 400.
 func isLeapYear(year int) bool {
 	if year%4 == 0 {
 		if year%100 == 0 && year%400 != 0 {
@@ -8,6 +10,22 @@ func isLeapYear(year int) bool {
 		return true
 	}
 	return false
+}
+
+// Given a year, and a month (1-12) this function returns the count of days in that month. It uses
+// the year to determine if February should get an extra day because it's a leap year.
+func getDaysInMonth(year int, month int) int {
+	switch month {
+	case 2: // February
+		if isLeapYear(year) {
+			return 29
+		}
+		return 28
+	case 4, 6, 9, 11:
+		return 30
+	default:
+		return 31
+	}
 }
 
 func main() {}
