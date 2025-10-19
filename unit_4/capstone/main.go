9fac1dfdc979a0507de7f54ebd0dd386e1ead7a6
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-10-19 16:00:18 -0400

    Set up the unit 4 capstone file

---
+++ unit_4/capstone/main.go
@@ -0,0 +1,10 @@
+// For this challenge, you will build a simulation of underpopulation, overpopulation, and
+// reproduction called Conway's Game of Life (see https://mng.bz/xOyY). The simulation is played
+// out on a two-dimensional grid of cells. As such, this challenge focuses on slices.
+
+// Each cell has eight adjacent cells in the horizontal, vertical, and diagonal directions. In each
+// generation, cells live or die based on the number of living neighbors.
+package main
+
+func main() {
+}
