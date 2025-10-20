3d208eea0249205ae6a646e8b8d0d1b2d8adaf5d
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-10-19 20:57:52 -0400

    Completed 20.1.1 - looking at the universe

--- unit_4/capstone/main.go
+++ unit_4/capstone/main.go
@@ -6,5 +6,44 @@
 // generation, cells live or die based on the number of living neighbors.
 package main
 
+import "fmt"
+
+const (
+	// width of the universe
+	width = 80
+	// height of the universe
+	height = 15
+)
+
+type Universe [][]bool
+
+func NewUniverse() Universe {
+	var universe Universe
+	for range width {
+		column := make([]bool, height)
+		universe = append(universe, column)
+	}
+	return universe
+}
+
+func (u Universe) Show() {
+	const (
+		liveCell = '*'
+		deadCell = ' '
+	)
+	for y := range height {
+		for x := range width {
+			if u[x][y] {
+				fmt.Printf("%c", liveCell)
+			} else {
+				fmt.Printf("%c", deadCell)
+			}
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
+	universe := NewUniverse()
+	universe.Show()
 }
