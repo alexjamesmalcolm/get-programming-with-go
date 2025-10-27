2b0f0b5499b1a008ffab12ccbd406cfcc9993135
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-10-26 22:26:59 -0400

    Used error variables and created a validDigit error checker

--- unit_6/lesson_28/sudoku/main.go
+++ unit_6/lesson_28/sudoku/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
 
 const rows, columns = 9, 9
 
+var (
+	ErrBounds = errors.New("outside of grid boundaries")
+	ErrDigit  = errors.New("invalid digit")
+)
+
 // Grid is a Sudoku grid
 type Grid [rows][columns]int8
 
 func (g Grid) Set(row, column int, digit int8) error {
 	if !inBounds(row, column) {
-		return fmt.Errorf("(%v, %v) is outside of grid boundaries", row, column)
+		return ErrBounds
+	}
+	if !validDigit(digit) {
+		return ErrDigit
 	}
 	g[row][column] = digit
 	return nil
@@ -28,9 +37,13 @@ func inBounds(row, column int) bool {
 	return true
 }
 
+func validDigit(digit int8) bool {
+	return digit >= 1 && digit <= 9
+}
+
 func main() {
 	var g Grid
-	err := g.Set(0, 0, 5)
+	err := g.Set(0, 0, 10)
 	if err != nil {
 		fmt.Printf("An error occurred: %v.\n", err)
 		os.Exit(1)
